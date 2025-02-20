f81d920a2e9fa889a78ca1550863079380aa466f
Shaco-C <chuxuebushi@gmail.com>
2025-02-20 20:56:56 +0800

    交易微服务的拆分

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -3,16 +3,25 @@ package com.hmall.api.client;
 
 
 import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
 import java.util.List;
 
-@FeignClient(name = "item-service")
+@FeignClient("item-service")
 public interface ItemClient {
 
     @GetMapping("/item")
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+
+    @PutMapping("/item/stock/deduct")
+    void deductStock(@RequestBody List<OrderDetailDTO> items);
+
 }
