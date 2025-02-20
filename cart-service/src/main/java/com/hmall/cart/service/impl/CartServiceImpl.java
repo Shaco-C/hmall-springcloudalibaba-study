8356c584e6dac5d58d174890edf023d7efce2519
Shaco-C <chuxuebushi@gmail.com>
2025-02-20 14:01:29 +0800

    使用Openfeign和loadbalance修改cart-service

--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.cart.client.ItemClient;
 import com.hmall.cart.domain.dto.CartFormDTO;
 import com.hmall.cart.domain.dto.ItemDTO;
 import com.hmall.cart.domain.po.Cart;
@@ -44,10 +45,12 @@ import java.util.stream.Collectors;
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
 
 //    private final IItemService itemService;
+//
+//    private final RestTemplate restTemplate;
+//
+//    private final DiscoveryClient discoveryClient;
 
-    private final RestTemplate restTemplate;
-
-    private final DiscoveryClient discoveryClient;
+    private final ItemClient itemClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -95,29 +98,31 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
         // 2.查询商品
 
-        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
-        if (CollUtils.isEmpty(instances)) {
-            return;
-        }
+//        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+//        if (CollUtils.isEmpty(instances)) {
+//            return;
+//        }
+//
+//        ServiceInstance serviceInstance = instances.get(RandomUtil.randomInt(instances.size()));
+//
+//
+//        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
+//                serviceInstance.getUri()+"/items?ids={ids}",
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<ItemDTO>>() {
+//                },
+//                Map.of("ids", CollUtil.join(itemIds, ",")
+//                ));
+//
+//        //判断查询是否成功
+//        if (!response.getStatusCode().is2xxSuccessful()){
+//            //查询失败则直接结束
+//            return;
+//        }
+//        List<ItemDTO> items = response.getBody();
 
-        ServiceInstance serviceInstance = instances.get(RandomUtil.randomInt(instances.size()));
-
-
-        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                serviceInstance.getUri()+"/items?ids={ids}",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<ItemDTO>>() {
-                },
-                Map.of("ids", CollUtil.join(itemIds, ",")
-                ));
-
-        //判断查询是否成功
-        if (!response.getStatusCode().is2xxSuccessful()){
-            //查询失败则直接结束
-            return;
-        }
-        List<ItemDTO> items = response.getBody();
+        List<ItemDTO> items = itemClient.queryItemByIds(itemIds);
 
         if (CollUtils.isEmpty(items)) {
             return;
