cd643d2707f846e33786dce3dbca2ce19dfea1f4
Shaco-C <chuxuebushi@gmail.com>
2025-02-21 22:58:16 +0800

    Openfeign传递用户信息，通过RequestInterceptor。

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,11 +1,29 @@
 package com.hmall.api.config;
 
+import cn.hutool.core.util.StrUtil;
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor userInfoInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                String userInfo = UserContext.getUser().toString();
+                if (StrUtil.isNotBlank(userInfo)){
+                    requestTemplate.header("user-info", userInfo);
+                }
+
+            }
+        };
     }
 }
