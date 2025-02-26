a31c796e99b3cfeda25f9f9c4f65f8332d7817a4
Shaco-C <chuxuebushi@gmail.com>
2025-02-26 18:39:39 +0800

    ElasticSearch--JavaRestClient 批量操作，将数据库信息写入到es中

--- item-service/src/test/java/com/hmall/item/es/ElasticDocumentTest.java
+++ item-service/src/test/java/com/hmall/item/es/ElasticDocumentTest.java
@@ -1,18 +1,24 @@
 package com.hmall.item.es;
 
 import cn.hutool.core.bean.BeanUtil;
+
 import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmall.item.domain.po.Item;
 import com.hmall.item.domain.po.ItemDoc;
 import com.hmall.item.service.IItemService;
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.GetIndexRequest;
+
 import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,7 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
-
+import java.util.List;
 
 
 @SpringBootTest(properties = "spring.profiles.active=local")
@@ -32,6 +38,8 @@ public class ElasticDocumentTest {
     @Autowired
     private IItemService itemService;
 
+
+    //是新增的方法，也是全量更新的方法
     @Test
     void createIndexDoc() throws IOException {
         //0.准备文档数据
@@ -47,6 +55,77 @@ public class ElasticDocumentTest {
         //3.发送请求
         client.index(request, RequestOptions.DEFAULT);
 
+    }
+
+    @Test
+    void testGetDoc() throws IOException {
+        GetRequest getRequest = new GetRequest("items", "1");
+
+        GetResponse response = client.get(getRequest, RequestOptions.DEFAULT);
+
+        String source = response.getSourceAsString();
+
+        ItemDoc itemDoc = JSONUtil.toBean(source, ItemDoc.class);
+
+        System.out.println(itemDoc);
+    }
+
+    @Test
+    void testDeleteDoc() throws IOException {
+        DeleteRequest getRequest = new DeleteRequest("items", "1");
+
+        client.delete(getRequest, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testUpdateDoc() throws IOException {
+        UpdateRequest updateRequest = new UpdateRequest("items","1");
+
+        updateRequest.doc(
+                "price",200,
+                "name","测试商品"
+        );
+
+        client.update(updateRequest, RequestOptions.DEFAULT);
+    }
+
+
+    //将数据库中的信息写入到es中
+    @Test
+    void testBulk() throws IOException {
+
+        int pageNo=1;
+        int pageSize=500;
+
+        while (true){
+
+            //1.准备文档数据
+            Page<Item> itemPage = itemService.lambdaQuery().eq(Item::getStatus, 1).page(Page.of(pageNo, pageSize));
+
+            List<Item> items = itemPage.getRecords();
+
+            if (items == null || items.isEmpty()){
+                return;
+            }
+
+            BulkRequest bulkRequest = new BulkRequest();
+
+//        bulkRequest.add(new IndexRequest("items").id("1").source("json", XContentType.JSON));
+////        bulkRequest.add(new IndexRequest("items").id("1").source("json", XContentType.JSON));
+////        bulkRequest.add(new IndexRequest("items").id("1").source("json", XContentType.JSON));
+////        bulkRequest.add(new IndexRequest("items").id("1").source("json", XContentType.JSON));
+
+            for (Item item : items) {
+                bulkRequest.add(new IndexRequest("items")
+                        .id(item.getId().toString())
+                        .source(JSONUtil.toJsonStr(BeanUtil.copyProperties(item, ItemDoc.class)), XContentType.JSON));
+
+            }
+            client.bulk(bulkRequest, RequestOptions.DEFAULT);
+            pageNo++;
+        }
+
+        //翻页
     }
 
     @BeforeEach
